748cec720bcea86dd139570f993fd9bcdb0e2758
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2021-02-14 01:05:14 +0530

    Cleanup variable names

--- gisys.py
+++ gisys.py
@@ -6,35 +6,35 @@ import subprocess
 from datetime import datetime
 
 def sysinfo(measure):
-	SYS_load = [load/psutil.cpu_count()*100 for load in psutil.getloadavg()]
-	CPU_temp = float(psutil.sensors_temperatures()['cpu_thermal'][0][1])
-	RAM_used = float(psutil.virtual_memory().percent)
-	DSK_root = float(psutil.disk_usage('/').percent)
+	sys_load = [load/psutil.cpu_count()*100 for load in psutil.getloadavg()]
+	cpu_temp = float(psutil.sensors_temperatures()['cpu_thermal'][0][1])
+	ram_used = float(psutil.virtual_memory().percent)
+	dsk_root = float(psutil.disk_usage('/').percent)
 	
 	DATASET = [{
 		"measurement": measure,
 		"time": epochs,
 		"fields":{
-			"sys_load_1m" : SYS_load[0],
-			"sys_load_5m" : SYS_load[1],
-			"sys_load_15m": SYS_load[2],
-			"cpu_temp": CPU_temp,
-			"ram_used": RAM_used,
-			"dsk_used": DSK_root
+			"sys_load_1m" : sys_load[0],
+			"sys_load_5m" : sys_load[1],
+			"sys_load_15m": sys_load[2],
+			"cpu_temp": cpu_temp,
+			"ram_used": ram_used,
+			"dsk_used": dsk_root
 		}
 	}]
 	print(DATASET)
 
 def netinfo(measure):
-	RESPONSE = callSubProc("netstat -s")
+	response = callSubProc("netstat -s")
 
-	IP_now   = int(re.findall("(\d+) total packets received", RESPONSE, re.MULTILINE)[0])
-	ICMP_now = int(re.findall("(\d+) ICMP messages received", RESPONSE, re.MULTILINE)[0])
-	ICMP_now+= int(re.findall("(\d+) ICMP messages sent", RESPONSE, re.MULTILINE)[0])
-	TCP_now  = int(re.findall("(\d+) segments received", RESPONSE, re.MULTILINE)[0])
-	TCP_now += int(re.findall("(\d+) segments sent out", RESPONSE, re.MULTILINE)[0])
-	UDP_now  = int(re.findall("(\d+) packets received", RESPONSE, re.MULTILINE)[0])
-	UDP_now += int(re.findall("(\d+) packets sent", RESPONSE, re.MULTILINE)[0])
+	ip_now   = int(re.findall("(\d+) total packets received", response, re.MULTILINE)[0])
+	icmp_now = int(re.findall("(\d+) ICMP messages received", response, re.MULTILINE)[0])
+	icmp_now+= int(re.findall("(\d+) ICMP messages sent", response, re.MULTILINE)[0])
+	tcp_now  = int(re.findall("(\d+) segments received", response, re.MULTILINE)[0])
+	tcp_now += int(re.findall("(\d+) segments sent out", response, re.MULTILINE)[0])
+	udp_now  = int(re.findall("(\d+) packets received", response, re.MULTILINE)[0])
+	udp_now += int(re.findall("(\d+) packets sent", response, re.MULTILINE)[0])
 
 	DATASET = [{
 		"measurement": measure,
@@ -43,10 +43,10 @@ def netinfo(measure):
 			"metric": "total"
 		},
 		"fields":{
-			"IP"   : IP_now,
-			"ICMP" : ICMP_now,
-			"TCP"  : TCP_now,
-			"UDP"  : UDP_now
+			"IP"   : ip_now,
+			"ICMP" : icmp_now,
+			"TCP"  : tcp_now,
+			"UDP"  : udp_now
 		}
 	}]
 	print(DATASET)
@@ -60,9 +60,9 @@ def pingtest(measure):
 
 	for tag, IPset in IPaddresses.items():
 		i, ip, c = IPset
-		RESPONSE = callSubProc("ping "+ip+" -c "+c+" | awk '/rtt/{print $4}'")
-		if RESPONSE:
-			ping = float(RESPONSE.split('/')[0])  #Select ping metric: [0]=min,[1]=avg,[2]=max,[3]=mdev
+		response = callSubProc("ping "+ip+" -c "+c+" | awk '/rtt/{print $4}'")
+		if response:
+			ping = float(response.split('/')[0])  #Select ping metric: [0]=min,[1]=avg,[2]=max,[3]=mdev
 			DATASET[0]['fields'][tag] = ping
 		elif i>0 : break
 	print(DATASET)
