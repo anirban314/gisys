32666c01340e164cd12ea7de86899bb161a2fcc4
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2021-02-14 01:59:17 +0530

    Update function to check dependencies
    Updated the checkDependencies() function to display the modules and/or
    packages to install, if not already installed.

--- gisys-config.py
+++ gisys-config.py
@@ -1,3 +1,4 @@
+import sys
 import importlib
 import subprocess
 
@@ -6,20 +7,42 @@ def checkDependencies():
 		'psutil': False
 	}
 	bash_packages = {
-		'awk': 'gawk',
-		'netstat': 'net-tools',
-		'ping': 'iputils-ping'
+		'awk': False,
+		'netstat': False,
+		'ping': False
 	}
+
 	for module in pypi_modules.keys():
 		try: importlib.import_module(module)
 		except: pass
 		else: pypi_modules[module] = True
-	for package in bash_packages.keys():
-		response = subprocess.run(['which', package], capture_output=True).stdout
-		if response: bash_packages[package] = True
 
-	print(pypi_modules)
-	print(bash_packages)
+	for package in bash_packages.keys():
+		if subprocess.run(['which', package], capture_output=True).stdout:
+			bash_packages[package] = True
+
+	pypi_installed = all(pypi_modules.values())
+	bash_installed = all(bash_packages.values())
+
+	if not pypi_installed:
+		print("Please install the following Python modules:")
+		for module, flag in pypi_modules.items():
+			if not flag:
+				print(" -> ", module)
+	else:
+		print("Required Python modules installed...")
+
+	if not bash_installed:
+		print("Please install the following Linux packages:")
+		for package, flag in bash_packages.items():
+			if not flag:
+				print(" -> ", package)
+	else:
+		print("Required Linux packages installed...")
+
+	return pypi_installed and bash_installed
 
 if __name__ == "__main__":
-	checkDependencies()
+	if not checkDependencies():
+		print("Quitting!")
+		sys.exit(1)
