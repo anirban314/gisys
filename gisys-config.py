a4326d90c7aff81965d3b09ee9b3d274c62e8cde
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2021-02-14 01:13:29 +0530

    Add function to check dependencies

--- gisys-config.py
+++ gisys-config.py
@@ -1,23 +1,25 @@
 import importlib
 import subprocess
 
-pypi_modules = {
-	'psutil': False
-}
-bash_packages = {
-	'awk': 'gawk',
-	'netstat': 'net-tools',
-	'ping': 'iputils-ping'
-}
+def checkDependencies():
+	pypi_modules = {
+		'psutil': False
+	}
+	bash_packages = {
+		'awk': 'gawk',
+		'netstat': 'net-tools',
+		'ping': 'iputils-ping'
+	}
+	for module in pypi_modules.keys():
+		try: importlib.import_module(module)
+		except: pass
+		else: pypi_modules[module] = True
+	for package in bash_packages.keys():
+		response = subprocess.run(['which', package], capture_output=True).stdout
+		if response: bash_packages[package] = True
 
-for module in pypi_modules.keys():
-	try: importlib.import_module(module)
-	except: pass
-	else: pypi_modules[module] = True
+	print(pypi_modules)
+	print(bash_packages)
 
-for package in bash_packages.keys():
-	response = subprocess.run(['which', package], capture_output=True).stdout
-	if response: bash_packages[package] = True
-
-print(pypi_modules)
-print(bash_packages)
+if __name__ == "__main__":
+	checkDependencies()
