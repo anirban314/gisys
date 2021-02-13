4328f5b06154ddbb130535fddbf148374a64e96d
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2021-02-13 19:05:57 +0530

    Add functionality to verify dependencies
    Added code to verify required Python modules and Linux packages exists
    on the system. This will be put into a function later.

--- gisys-config.py
+++ gisys-config.py
@@ -1,10 +1,23 @@
+import importlib
 import subprocess
 
 pypi_modules = {
 	'psutil': False
 }
 bash_packages = {
+	'awk': 'gawk',
 	'netstat': 'net-tools',
-	'ping': 'iputils-ping',
-	'awk': 'gawk'
+	'ping': 'iputils-ping'
 }
+
+for module in pypi_modules.keys():
+	try: importlib.import_module(module)
+	except: pass
+	else: pypi_modules[module] = True
+
+for package in bash_packages.keys():
+	response = subprocess.run(['which', package], capture_output=True).stdout
+	if response: bash_packages[package] = True
+
+print(pypi_modules)
+print(bash_packages)
