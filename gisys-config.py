c60fff119a3eea01d4e3b415d0394060f62b5aaa
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2021-02-20 01:15:36 +0530

    Optimized code to check if dependencies are installed

--- gisys-config.py
+++ gisys-config.py
@@ -13,37 +13,31 @@ def checkDependencies():
 		'ping': False
 	}
 
+	print("CHECKING DEPENDENCIES...")
+
 	for module in pypi_modules.keys():
-		try: importlib.import_module(module)
-		except: pass
-		else: pypi_modules[module] = True
+		print("  Python Module:\t{}  \t".format(module), end='')
+		try:
+			importlib.import_module(module)
+		except:
+			print("not installed")
+		else:
+			print("installed")
+			pypi_modules[module] = True
+	pypi_installed = all(pypi_modules.values())
 
 	for package in bash_packages.keys():
-		if subprocess.run(['which', package], capture_output=True).stdout:
+		print("  Linux Package:\t{}     \t".format(package), end='')
+		if not subprocess.run(['which', package], capture_output=True).stdout:
+			print("not installed")
+		else:
+			print("installed")
 			bash_packages[package] = True
-
-	pypi_installed = all(pypi_modules.values())
 	bash_installed = all(bash_packages.values())
-
-	if not pypi_installed:
-		print("Please install the following Python modules:")
-		for module, flag in pypi_modules.items():
-			if not flag:
-				print(" -> ", module)
-	else:
-		print("Required Python modules installed...")
-
-	if not bash_installed:
-		print("Please install the following Linux packages:")
-		for package, flag in bash_packages.items():
-			if not flag:
-				print(" -> ", package)
-	else:
-		print("Required Linux packages installed...")
-
+	
 	return pypi_installed and bash_installed
 
 if __name__ == "__main__":
 	if not checkDependencies():
-		print("Quitting!")
+		print("\nQuitting!")
 		sys.exit(1)
