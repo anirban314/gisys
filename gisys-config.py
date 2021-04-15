3f737785e83994d656f098b599c22ba7f9ccb0e3
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2021-04-15 14:57:30 +0530

    Add functionality to install missing dependencies

--- gisys-config.py
+++ gisys-config.py
@@ -13,7 +13,7 @@ def checkDependencies():
 		'ping': False
 	}
 
-	print("CHECKING DEPENDENCIES...")
+	print("Checking Dependencies...\n")
 
 	for module in pypi_modules.keys():
 		print("  Python Module:\t{}  \t".format(module), end='')
@@ -34,8 +34,30 @@ def checkDependencies():
 			print("installed")
 			bash_packages[package] = True
 	bash_installed = all(bash_packages.values())
+
+	if not pypi_installed or not bash_installed:
+		user_choice = input("\nDependencies Missing!\nDo you want to install them now (yes/no)? ").lower()
+		if user_choice=='y' or user_choice=='yes':
+			installDependencies(pypi_modules, bash_packages)
+		else:
+			return False
 	
-	return pypi_installed and bash_installed
+	return True
+
+def installDependencies(pypi_modules, bash_packages):
+	install_command = ['sudo', 'pip', 'install']
+	for module, installed in pypi_modules.items():
+		if not installed:
+			install_command.append(module)
+	if len(install_command) > 3:
+		subprocess.run(install_command)
+	
+	install_command = ['sudo', 'apt-get', 'install', '--yes']
+	for package, installed in bash_packages.items():
+		if not installed:
+			install_command.append(module)
+	if len(install_command) > 4:
+		subprocess.run(install_command)
 
 if __name__ == "__main__":
 	if not checkDependencies():
