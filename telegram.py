83f06f11567758ec7077753002b82f883a744df2
Anirban Mukherjee <37843481+debug314@users.noreply.github.com>
2022-03-27 16:25:53 +0530

    Remove alert_user function from 'telegram' module

--- telegram.py
+++ telegram.py
@@ -4,28 +4,6 @@
 
 import requests
 
-def alert_user(sys_, client, epochs):
-    load_1m = sys_['load_1m']
-    load_5m = sys_['load_5m']
-    load_15m = sys_['load_15m']
-    cpu_temp = sys_['cpu_temp']
-    ram_used = sys_['ram_used']
-    dsk_used = sys_['dsk_used']
-
-    message = f"::: SYSTEM ALERT\n::: Reporting for {client}\n"
-    if cpu_temp >= 60: #X#
-        message+= f"\nCPU Temperature at {cpu_temp}\u2103"
-    if ram_used >= 80: #X#
-        message+= f"\nMemory Usage at {ram_used}%"
-    if dsk_used >= 80: #X#
-        message+= f"\nRoot Disk Usage at {dsk_used}%"
-
-    message+= f"\n\nLoad Average (1 min.) at {load_1m}%"
-    message+= f"\nLoad Average (5 min.) at {load_5m}%"
-    message+= f"\nLoad Average (15 min.) at {load_15m}%"
-    message += f"\n\nEvent Epoch (sec.): {epochs}"
-    send_message(message)
-
 def send_message(message):
     token = '' #X#
     chat_id = '' #X#
